6fb41c8f1d6ad9410230db0e8ba10cf6951a692b
GLRoylance <GLRoylance@gmail.com>
2026-01-15 15:24:56 -0800

    update PIDF code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.MotorControlAlgorithm;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -10,33 +11,50 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 public class Shooter {
     // the shooter motor
     DcMotorEx motor;
+    double ticksPerRev;
+    double ticksPerSecondMax;
 
     public Shooter(HardwareMap hardwareMap) {
         // get the motor
         motor = hardwareMap.get(DcMotorEx.class, "motorShoot");
 
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Optional: Set custom PIDF values
-        PIDFCoefficients pidf = new PIDFCoefficients(
-                10.0,
-                3.0,
-                0.0,
-                13.5);
+        double p = 10.0;
+        double i =  3.0;
+        double d = 0.0;
+        ticksPerRev = motor.getMotorType().getTicksPerRev();
+        // TODO: ticksPerRev is 560.00
+        // probably gear ratio of 20 and 28 ticks per rev
+        ticksPerRev = 28;
+        // this is 2830. makes sense.
+        ticksPerSecondMax = motor.getMotorType().getAchieveableMaxTicksPerSecond();
+
+        // double f = 13.5;
+        // expect to be about 32000 / 100 * 28 approx 10
+        double f = 32000.0 / ticksPerSecondMax;
+        PIDFCoefficients pidf = new PIDFCoefficients(p, i, d, f, MotorControlAlgorithm.PIDF);
 
         motor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidf);
 
         motor.setPower(1.0);
 
-        motor.setVelocity(0.0);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        motor.setVelocity(0.0);
     }
 
+    /**
+     * Set the shooter velocity.
+     * @param rps
+     */
     public void setRPM(double rps) {
-        motor.setVelocity(rps, AngleUnit.RADIANS);
+        // chose a decent speed to set the speed.
+        // the native units are ticks per second
+        motor.setVelocity(rps * ticksPerRev);
     }
 }
